3816e798aaf90b2ccd3c555131624efcd6dcc60a
AnelCC <anel.calvo.cervantes@gmail.com>
2022-01-04 10:58:32 -0500

    SignupScreen UI

--- app/src/main/java/com/anelcc/instagram/auth/SignupScreen.kt
+++ app/src/main/java/com/anelcc/instagram/auth/SignupScreen.kt
@@ -1,12 +1,82 @@
 package com.anelcc.instagram.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.anelcc.instagram.IgViewModule
+import com.anelcc.instagram.R
 
 
 @Composable
 fun SignupScreen(navController: NavController, viewModule: IgViewModule) {
-    Text(text = "SignupScreen")
+    Box(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            val usernameState = remember { mutableStateOf(TextFieldValue()) }
+            val emailState = remember { mutableStateOf(TextFieldValue()) }
+            val passState = remember { mutableStateOf(TextFieldValue()) }
+            Image(painter = painterResource(id = R.drawable.ig_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(text = "signUp",
+                Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") })
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") })
+            OutlinedTextField(
+                value = passState.value,
+                onValueChange = { passState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") })
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.padding(8.dp)) {
+                Text(text = "Sing Up")
+            }
+
+
+            Text(text = "Are you already an User? Go To Login ->",
+                color = Color.Blue,
+                modifier = Modifier.padding(8.dp).clickable {
+
+                }
+            )
+        }
+    }
 }
