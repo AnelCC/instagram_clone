234695f537c3ca2ae7681bf5f424a3445096fb5d
AnelCC <anel.calvo.cervantes@gmail.com>
2022-01-17 19:39:11 -0500

    User Image Card

--- app/src/main/java/com/anelcc/instagram/main/PostScreen.kt
+++ app/src/main/java/com/anelcc/instagram/main/PostScreen.kt
@@ -1,21 +1,23 @@
 package com.anelcc.instagram.main
 
-import android.view.View
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.ButtonElevation
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.anelcc.instagram.R
 import com.anelcc.instagram.InstagramViewModel
 
 
@@ -96,5 +98,30 @@ fun PostScreen(navController: NavController, viewModel: InstagramViewModel) {
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
+    Box(modifier = Modifier
+        .padding(top = 16.dp)
+        .clickable { onClick.invoke() }) {
 
+        UserImageCard(
+            userImage = imageUrl, modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
+
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(Color.Blue)
+            )
+        }
+    }
 }
